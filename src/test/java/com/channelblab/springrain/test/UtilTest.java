e8ce0ffec17c648f03b43b04e74f9c980555117e
dengyi <yi.deng@minthgroup.com>
2024-07-12 17:03:31 +0800

    add test

---
+++ src/test/java/com/channelblab/springrain/test/UtilTest.java
@@ -0,0 +1,10 @@
+package com.channelblab.springrain.test;
+
+/**
+ * @author     ：dengyi(A.K.A Bear)
+ * @date       ：Created in 2024-07-12 17:03
+ * @description：
+ * @modified By：
+ */
+public class UtilTest {
+}
