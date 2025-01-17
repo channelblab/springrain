44d6d329b579a1eb622a30e43ae6fc347c6f3a66
dengyi <yi.deng@minthgroup.com>
2025-01-17 15:04:51 +0800

    修复日志记录

--- src/main/java/com/channelblab/springrain/common/anotations/NoLog.java
+++ src/main/java/com/channelblab/springrain/common/anotations/NoLog.java
@@ -7,7 +7,4 @@ import java.lang.annotation.*;
 @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NoLog {
-    String[] fieldNames() default {};
-
-
 }
